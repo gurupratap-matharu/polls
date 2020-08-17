854fa536c4c05eefc59aa15bde8547e514a713f8
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-17 16:32:00 -0300

    endpoints to view list and detail view

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from polls.views import HomePageView
+from polls.views import (HomePageView, QuestionDetailView, QuestionListView,
+                         ResultsView, VoteView)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('questions/', QuestionListView.as_view(), name='question_list'),
+    path('questions/<int:pk>/', QuestionDetailView.as_view(), name='question_detail'),
+    path('questions/<int:pk>/results/', ResultsView.as_view(), name='results'),
+    path('questions/<int:pk>/vote/', VoteView.as_view(), name='vote'),
 ]
