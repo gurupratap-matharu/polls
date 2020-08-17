b95fe3ad455c1951f4d240dee7125f34d396c390
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-17 16:32:16 -0300

    class based generic views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,27 @@
-from django.views.generic import TemplateView
+from django.views.generic import DetailView, ListView, TemplateView
+
+from polls.models import Question
 
 
 class HomePageView(TemplateView):
     template_name = 'polls/home.html'
+
+
+class QuestionListView(ListView):
+    model = Question
+    context_object_name = 'question_list'
+    template_name = 'polls/question_list.html'
+
+
+class QuestionDetailView(DetailView):
+    model = Question
+    context_object_name = 'question'
+    template_name = 'polls/question_detail.html'
+
+
+class VoteView(TemplateView):
+    template_name = 'polls/vote.html'
+
+
+class ResultsView(TemplateView):
+    template_name = 'polls/results.html'
