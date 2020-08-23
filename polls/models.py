5678cd8da7383306ca6d3b4866612be6d3258906
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-23 16:41:12 -0300

    added uuid as a primary key

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,5 @@
 import datetime
+import uuid
 
 from django.db import models
 from django.urls import reverse
@@ -6,6 +7,7 @@ from django.utils import timezone
 
 
 class Question(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
@@ -20,6 +22,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
