1afb03e399bfc023543f7dd597d4e480f768a08f
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-23 17:02:08 -0300

    added CRUD views for question model

--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
-from polls.views import (QuestionDetailView, QuestionListView, ResultsView,
+from polls.views import (QuestionCreate, QuestionDelete, QuestionDetailView,
+                         QuestionListView, QuestionUpdate, ResultsView,
                          VoteView)
 
 app_name = 'polls'
 urlpatterns = [
     path('', QuestionListView.as_view(), name='question_list'),
-    path('<int:pk>/', QuestionDetailView.as_view(), name='question_detail'),
-    path('<int:pk>/results/', ResultsView.as_view(), name='results'),
-    path('<int:pk>/vote/', VoteView.as_view(), name='vote'),
+    path('<uuid:pk>/', QuestionDetailView.as_view(), name='question_detail'),
+    path('create/', QuestionCreate.as_view(), name='question_create'),
+    path('<uuid:pk>/update/', QuestionUpdate.as_view(), name='question_update'),
+    path('<uuid:pk>/delete/', QuestionDelete.as_view(), name='question_delete'),
+    path('<uuid:pk>/results/', ResultsView.as_view(), name='results'),
+    path('<uuid:pk>/vote/', VoteView.as_view(), name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,8 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView, TemplateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from polls.models import Question
 
@@ -13,6 +17,29 @@ class QuestionDetailView(DetailView):
     model = Question
     context_object_name = 'question'
     template_name = 'polls/question_detail.html'
+
+
+class QuestionCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Question
+    fields = ['question_text']
+    success_message = "Question created successfully!"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class QuestionUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Question
+    fields = ['question_text']
+    template_name_suffix = '_update_form'
+    success_message = 'Question updated successfully!'
+
+
+class QuestionDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    model = Question
+    success_url = reverse_lazy('question_list')
+    success_message = 'Question deleted successfully!'
 
 
 class VoteView(TemplateView):
