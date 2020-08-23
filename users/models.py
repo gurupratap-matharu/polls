ad0aa7f98e5e5c4951b84f230b406af37dbd59bc
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-23 15:37:42 -0300

    added profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.urls import reverse
+
+
+class CustomUser(AbstractUser):
+    pass
+
+
+class Profile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    bio = models.CharField(max_length=600, blank=True)
+    location = models.CharField(max_length=200, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+    def get_absolute_url(self):
+        return reverse('profile', args=[str(self.id)])
