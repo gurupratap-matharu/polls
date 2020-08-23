36285bac345f561c29ad8c1f806a46dca28c908a
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-23 16:43:03 -0300

    added uuid as a primary key

--- polls/models.py
+++ polls/models.py
@@ -10,6 +10,7 @@ class Question(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.question_text
@@ -26,6 +27,10 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.choice_text
+
+    def get_absolute_url(self):
+        return reverse('choice_detail', args=str([self.id]))
