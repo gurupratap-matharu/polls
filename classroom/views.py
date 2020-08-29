1bb569c35901613d2fc5ccc021da98421633cf00
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-28 22:46:10 -0300

    added detail view with login required mixin

--- classroom/views.py
+++ classroom/views.py
@@ -1,9 +1,16 @@
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView, ListView
 
 from classroom.models import Classroom
 
 
-class ClassroomListView(ListView):
+class ClassroomListView(LoginRequiredMixin, ListView):
     model = Classroom
     context_object_name = 'classroom_list'
     template_name = 'classroom/classroom_list.html'
+
+
+class ClassroomDetailView(LoginRequiredMixin, DetailView):
+    model = Classroom
+    context_object_name = 'classroom'
+    template_name = 'classroom/classroom_detail.html'
