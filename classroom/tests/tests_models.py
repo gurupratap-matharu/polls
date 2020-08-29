4211228cb4143adc0a2b5ef30d7e907bcdeee5de
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-29 20:34:11 -0300

    added test to check __str__ representation

--- classroom/tests/tests_models.py
+++ classroom/tests/tests_models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from classroom.models import Classroom
+
+
+class ClassroomModelTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='testuser@email.com',
+            password='testpass123'
+        )
+        self.classroom = Classroom.objects.create(
+            name='test_class',
+            section='test_section',
+            subject='test_subject',
+            room='test_room',
+            created_by=self.user
+        )
+
+    def test_classroom_listing(self):
+        self.assertEqual(f'{self.classroom.name}', 'test_class')
+        self.assertEqual(f'{self.classroom.section}', 'test_section')
+        self.assertEqual(f'{self.classroom.subject}', 'test_subject')
+        self.assertEqual(f'{self.classroom.room}', 'test_room')
+        self.assertEqual(self.classroom.created_by, self.user)
+        self.assertEqual(str(self.classroom), 'test_class')
