9073eaea5c935d3e22e37bcd712fb135e32ea6dc
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-29 22:40:54 -0300

    added endpoints for classroom update and delete

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from classroom.views import ClassroomDetailView, ClassroomListView
+from classroom.views import (ClassroomCreate, ClassroomDelete,
+                             ClassroomDetailView, ClassroomListView,
+                             ClassroomUpdate)
 
 urlpatterns = [
     path('', ClassroomListView.as_view(), name='classroom_list'),
     path('<uuid:pk>/', ClassroomDetailView.as_view(), name='classroom_detail'),
+    path('create/', ClassroomCreate.as_view(), name='classroom_create'),
+    path('<uuid:pk>/update/', ClassroomUpdate.as_view(), name='classroom_update'),
+    path('<uuid:pk>/delete/', ClassroomDelete.as_view(), name='classroom_delete'),
 ]
--- classroom/views.py
+++ classroom/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from classroom.models import Classroom
 
@@ -14,3 +17,26 @@ class ClassroomDetailView(LoginRequiredMixin, DetailView):
     model = Classroom
     context_object_name = 'classroom'
     template_name = 'classroom/classroom_detail.html'
+
+
+class ClassroomCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Classroom
+    fields = ['name', 'section', 'subject', 'room']
+    template_name = 'classroom/classroom_form.html'
+    success_message = "%(name)s successfully created!"
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+
+class ClassroomUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Classroom
+    fields = ['name', 'section', 'subject', 'room']
+    template_name = 'classroom/classroom_update_form.html'
+    success_message = "%(name)s successfully updated!"
+
+
+class ClassroomDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    model = Classroom
+    success_url = reverse_lazy('classroom_list')
