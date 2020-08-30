3031dd6957687094f898cde6b07a1de16ce08a3b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-30 13:54:10 -0300

    added unit tests for update and delete operations

--- classroom/tests/test_views.py
+++ classroom/tests/test_views.py
@@ -2,8 +2,11 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import resolve, reverse
 
+from classroom.factories import ClassroomFactory, UserFactory
 from classroom.models import Classroom
-from classroom.views import ClassroomDetailView, ClassroomListView
+from classroom.views import (ClassroomCreate, ClassroomDelete,
+                             ClassroomDetailView, ClassroomListView,
+                             ClassroomUpdate)
 
 
 class ClassroomListTests(TestCase):
@@ -75,12 +78,91 @@ class ClassroomDetailTests(TestCase):
 
 
 class ClassroomCreateTests(TestCase):
-    pass
+    def setUp(self):
+        self.user = UserFactory()
+        self.classroom = ClassroomFactory(created_by=self.user)
+
+    def test_classroom_create_view_redirects_for_anonymous_user(self):
+        response = self.client.get(reverse('classroom_create'))
+        no_response = self.client.get('/class/create/')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateNotUsed(response, 'classroom/classroom_form.html')
+        self.assertEqual(no_response.status_code, 404)
+
+    def test_classroom_create_works_for_logged_in_user(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('classroom_create'))
+        no_response = self.client.get('/class/create/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Create')
+        self.assertContains(response, 'name')
+        self.assertNotContains(response, 'Hi I should not be on this page!')
+        self.assertTemplateUsed(response, 'classroom/classroom_form.html')
+        self.assertEqual(no_response.status_code, 404)
+
+    def test_classroom_create_resolve_classroomcreateview(self):
+        view = resolve(reverse('classroom_create'))
+        self.assertEqual(view.func.__name__, ClassroomCreate.as_view().__name__)
 
 
 class ClassroomUpdateTests(TestCase):
-    pass
+    def setUp(self):
+        self.user = UserFactory()
+        self.classroom = ClassroomFactory(created_by=self.user)
+
+    def test_classroom_update_view_redirects_for_anonymous_user(self):
+        response = self.client.get(self.classroom.get_update_url())
+        no_response = self.client.get('/class/update/')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateNotUsed(response, 'classroom/classroom_update_form.html')
+        self.assertEqual(no_response.status_code, 404)
+
+    def test_classroom_update_works_for_logged_in_user(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.classroom.get_update_url())
+        no_response = self.client.get('/class/update/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Update')
+        self.assertContains(response, 'name')
+        self.assertContains(response, self.classroom.name)
+        self.assertNotContains(response, 'Hi I should not be on this page!')
+        self.assertTemplateUsed(response, 'classroom/classroom_update_form.html')
+        self.assertEqual(no_response.status_code, 404)
+
+    def test_classroom_update_resolve_classroomupdateview(self):
+        view = resolve(self.classroom.get_update_url())
+        self.assertEqual(view.func.__name__, ClassroomUpdate.as_view().__name__)
 
 
 class ClassroomDeleteTests(TestCase):
-    pass
+    def setUp(self):
+        self.user = UserFactory()
+        self.classroom = ClassroomFactory(created_by=self.user)
+
+    def test_classroom_delete_redirects_for_anonymous_user(self):
+        response = self.client.get(self.classroom.get_delete_url())
+        no_response = self.client.get('/classroom/123456/delete/')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateNotUsed(response, 'classroom/classroom_confirm_delete.html')
+        self.assertEqual(no_response.status_code, 404)
+
+    def test_classroom_delete_works_for_logged_in_user(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.classroom.get_delete_url())
+        no_response = self.client.get('/classroom/123456/delete/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'classroom/classroom_confirm_delete.html')
+        self.assertContains(response, 'Delete')
+        self.assertContains(response, 'Confirm')
+        self.assertNotContains(response, 'Hi I should not be on this page!')
+        self.assertEqual(no_response.status_code, 404)
+
+    def test_classroom_delete_resolves_classroomdeleteview(self):
+        view = resolve(self.classroom.get_delete_url())
+        self.assertEqual(view.func.__name__, ClassroomDelete.as_view().__name__)
