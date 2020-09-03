44ab6a7e211e5e338bc51809e8051d92582da87c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-09-03 19:08:12 -0300

    added enrollmentcreate view for enrolling any student

--- classroom/views.py
+++ classroom/views.py
@@ -2,11 +2,13 @@ import logging
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, FormView, ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
-from classroom.models import Classroom
+from classroom.forms import EnrollmentForm
+from classroom.models import Classroom, Enrollment
 
 logger = logging.getLogger(__name__)
 
@@ -51,3 +53,26 @@ class ClassroomDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Classroom
     success_url = reverse_lazy('classroom_list')
     success_message = "%(name)s successfully deleted!"
+
+
+class EnrollmentCreate(LoginRequiredMixin, SuccessMessageMixin, FormView):
+    template_name = 'classroom/enrollment_form.html'
+    success_message = 'Great! You have enrolled successfully.'
+    form_class = EnrollmentForm
+    success_url = reverse_lazy('classroom_list')
+
+    def form_valid(self, form):
+        code = self.request.POST['code']
+        student = self.request.user
+        classroom = Classroom.objects.get(id=code)
+
+        enrollment, created = Enrollment.objects.get_or_create(student=student, classroom=classroom)
+
+        if not created:
+            logger.critical('Student already enrolled. redirecting...')
+            self.success_message = 'You are already enrolled!'
+            redirect(reverse_lazy('classroom_list'))
+
+        logger.info('Enrolled in %s', enrollment)
+        form.send_mail(student=student, classroom=classroom)
+        return super().form_valid(form)
