dadf9742fa720a510ac369cc4f87706f1ced7f64
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-09-05 16:37:46 -0300

    customized the admin interface for classroom and enrollment models

--- classroom/admin.py
+++ classroom/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 
-from classroom.models import Classroom
+from classroom.models import Classroom, Enrollment
 
-admin.site.register(Classroom)
+
+class EnrollmentAdmin(admin.ModelAdmin):
+    model = Enrollment
+    list_display = ('student', 'classroom', 'date_joined', 'is_active', 'marks',)
+    list_filter = ('date_joined',)
+    search_fields = ('classroom__name',)
+
+
+class ClassroomAdmin(admin.ModelAdmin):
+    model = Classroom
+    list_display = ('name', 'section', 'subject', 'room', 'is_active', 'created_at', 'created_by',)
+    list_filter = ('is_active', 'created_at',)
+    list_editable = ('is_active',)
+    search_fields = ('name',)
+
+
+admin.site.register(Classroom, ClassroomAdmin)
+admin.site.register(Enrollment, EnrollmentAdmin)
