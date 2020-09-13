ee35c439c206d74a724632ab5251ef663dd82b0e
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-09-13 16:24:51 -0300

    removed dot-env as we are not using it

--- main/settings.py
+++ main/settings.py
@@ -13,9 +13,6 @@ import os
 from pathlib import Path
 
 import dj_database_url
-from dotenv import load_dotenv
-
-load_dotenv(verbose=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
