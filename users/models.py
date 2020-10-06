e731225712a4c122d28dbe3720abc2875a2a4941
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-06 10:19:03 -0300

    added helper methods on profile model

--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
+from django_countries.fields import CountryField
 
 
 class CustomUser(AbstractUser):
@@ -16,14 +17,15 @@ class Profile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     bio = models.CharField(max_length=600, blank=True)
     location = models.CharField(max_length=200, blank=True)
+    country = CountryField(blank_label='(select country)')
     birth_date = models.DateField(null=True, blank=True)
-    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='profile')
 
     def __str__(self):
-        return self.user
+        return self.bio
 
     def get_absolute_url(self):
-        return reverse('profile', args=[str(self.id)])
+        return reverse('profile_detail', args=[str(self.id)])
 
 
 @receiver(post_save, sender=get_user_model())
