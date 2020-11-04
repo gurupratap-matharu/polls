b31cdf91eeb08ec027e5b3551ac99a6f3c69c3c8
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-04 18:28:29 -0300

    added versioning to api

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from polls.views import QuestionDetailAPIView, QuestionListAPIView
 from users.views import (ProfileDetailAPIView, ProfileListAPIView,
                          UserDetailAPIView, UserListAPIView)
 
@@ -7,4 +8,6 @@ urlpatterns = [
     path('users/', UserListAPIView.as_view()),
     path('profile/', ProfileListAPIView.as_view()),
     path('profile/<uuid:pk>/', ProfileDetailAPIView.as_view()),
+    path('polls/', QuestionListAPIView.as_view()),
+    path('polls/<uuid:pk>/', QuestionDetailAPIView.as_view()),
 ]
