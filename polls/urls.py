6bd1f191aa42f86549df3e69fadbc24f8be4bfee
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-08 12:45:18 -0300

    reverted slug to uuid fields

--- polls/urls.py
+++ polls/urls.py
@@ -7,10 +7,10 @@ from polls.views import (QuestionCreate, QuestionDelete, QuestionDetailView,
 app_name = 'polls'
 urlpatterns = [
     path('', QuestionListView.as_view(), name='question_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:question>/', QuestionDetailView.as_view(), name='question_detail'),
+    path('<int:uuid>/', QuestionDetailView.as_view(), name='question_detail'),
     path('create/', QuestionCreate.as_view(), name='question_create'),
-    path('<int:year>/<int:month>/<int:day>/<slug:question>/update/', QuestionUpdate.as_view(), name='question_update'),
-    path('<int:year>/<int:month>/<int:day>/<slug:question>/delete/', QuestionDelete.as_view(), name='question_delete'),
-    path('<int:year>/<int:month>/<int:day>/<slug:question>/results/', ResultsView.as_view(), name='results'),
-    path('<int:year>/<int:month>/<int:day>/<slug:question>/vote/', VoteView.as_view(), name='vote'),
+    path('<int:uuid>/update/', QuestionUpdate.as_view(), name='question_update'),
+    path('<int:uuid>/delete/', QuestionDelete.as_view(), name='question_delete'),
+    path('<int:uuid>/results/', ResultsView.as_view(), name='results'),
+    path('<int:uuid>/vote/', VoteView.as_view(), name='vote'),
 ]
