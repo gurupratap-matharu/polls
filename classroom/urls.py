ea160996ac904262203cc0430f0579642e5f755c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-08 15:21:57 -0300

    endpoint for listing classrooms by tags

--- classroom/urls.py
+++ classroom/urls.py
@@ -7,11 +7,12 @@ from classroom.views import (ClassroomCreate, ClassroomDelete,
 
 urlpatterns = [
     path('', ClassroomListView.as_view(), name='classroom_list'),
+    path('tag/<slug:tag_slug>/', ClassroomListView.as_view(), name='classroom_list_by_tag'),
     path('<uuid:pk>/', ClassroomDetailView.as_view(), name='classroom_detail'),
-    path('<uuid:pk>/people/', ClassroomPeopleView.as_view(), name='classroom_people'),
-    path('create/', ClassroomCreate.as_view(), name='classroom_create'),
     path('<uuid:pk>/update/', ClassroomUpdate.as_view(), name='classroom_update'),
     path('<uuid:pk>/delete/', ClassroomDelete.as_view(), name='classroom_delete'),
+    path('<uuid:pk>/people/', ClassroomPeopleView.as_view(), name='classroom_people'),
+    path('create/', ClassroomCreate.as_view(), name='classroom_create'),
     path('enroll/', EnrollmentCreate.as_view(), name='enroll'),
     path('enroll/<uuid:pk>/delete/', EnrollmentDelete.as_view(), name='enroll_delete'),
 ]
